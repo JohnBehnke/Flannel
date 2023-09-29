899faa399ed84ced197f22e0548fc022507218c5
John Behnke <john@behnke.xyz>
2023-09-29 07:49:29 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Flannel",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Flannel",
+            targets: ["Flannel"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Flannel"),
+        .testTarget(
+            name: "FlannelTests",
+            dependencies: ["Flannel"]),
+    ]
+)
