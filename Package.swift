c5c79e8581c53fa4d5f2f95ea4ae01a328bfe74c
John Behnke <john@behnke.xyz>
2025-09-01 16:27:12 -0400

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version: 6.2// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
